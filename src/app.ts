b061ad20a5c6e0022682c456a9821b9c4ba71758
sunilkumar27 <sunilkumar27@gmail.com>
2025-02-24 15:15:42 +0530

    Fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,12 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 app.use(helmet());
 app.use(cors({
   origin: process.env.CLIENT_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-}));
+  optionsSuccessStatus: 200}
+));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
